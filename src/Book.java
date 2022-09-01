e8cc45d9b9939e34b75b1030dba64d7daf0636e4
HP <mdzaid1299@gmail.com>
2022-09-01 11:51:50 +0530

    ini

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class Book {
+
+public class Book {
+    private String bookName;
+    private String author;
+    private long isbn;
+    private double price;
+    public Book(){
+        bookName = "";
+        author = "";
+        isbn = 0;
+        price = 0.0;
+    }
+
+    public Book(String bookName, String author, long isbn, double price) {
+        this.bookName = bookName;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn=" + isbn +
+                ", price=" + price +
+                '}';
+    }
 }
+/*
+ *Create a List of 10 Book objects, where Book is a user defined or custom class.
+ * Book class contains following field String bookName,
+ *  String author, long isbn, & double price.
+ *  Book class must be properly encapsulated and must implement data hiding.
+ *  Book class contains parameterized constructors and no-argument constructor.
+ *  Book class must override String toString() of Object class to represent Book object as string while printing.
+ * Kindly sort the list of book by price.
+ */
--- src/BookImpl.java
+++ src/BookImpl.java
@@ -1,48 +1,47 @@
 
 import java.util.ArrayList;
-        import java.util.List;
+import java.util.List;
 
 public class BookImpl{
     public static void main(String[] args) {
-        Book obj1 = new Book("Java","Rajiv",456789923L,560);
-        Book obj2 = new Book("C","Mukesh",787892349L,230);
-        Book obj3 = new Book("C++","David",238279987L,340);
-        Book obj4 = new Book("javascript","Dinesh",1235435646L,670);
-        Book obj5 = new Book("HTML","Rajiv",456789923L,590);
+        Book obj1 = new Book("Java","vinay",76467423L,960);
+        Book obj2 = new Book("C","Mukesh",77645769L,630);
+        Book obj3 = new Book("CSharp","Vince",453542987L,740);
+        Book obj4 = new Book("snell","Nobody",23234135646L,570);
+        Book obj5 = new Book("HTML","Mudit",876785453L,990);
+
+//        System.out.println("obj1 = " + obj1);
+//        System.out.println("obj2 = " + obj2);
+//        System.out.println("obj3 = " + obj3);
+//        System.out.println("obj4 = " + obj4);
+//        System.out.println("obj5 = " + obj5);
 
 
-        System.out.println("obj5 = " + obj5);
-        System.out.println("obj4 = " + obj4);
-        System.out.println("obj3 = " + obj3);
-        System.out.println("obj2 = " + obj2);
-        System.out.println("obj1 = " + obj1);
-        List<Double> obj = new ArrayList<>();
-        obj.add(560.0);
-        obj.add(230.0);
-        obj.add(340.0);
-        obj.add(670.0);
-        obj.add(590.0);
-        obj.add(278.0);
-        obj.add(470.0);
-        obj.add(290.0);
-        obj.add(530.0);
-        obj.add(2390.0);
-
-        for (int i = 0; i < obj.size(); i++) {
-            for (int j = 0; j < obj.size() ; j++) {
-                if(obj.get(j) > obj.get(i)){
-                    double temp = obj.get(i);
-                    obj.set(i,obj.get(j));
-                    obj.set(j,temp);
+        List<Double> list = new ArrayList<>();
 
 
+        list.add(560.0);
+        list.add(230.0);
+        list.add(340.0);
+        list.add(670.0);
+        list.add(590.0);
+        
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int k = 0; k < list.size() ; k++) {
+                if(list.get(k) > list.get(i)){
+                    double temp = list.get(i);
+                    list.set(i,list.get(k));
+                    list.set(k,temp);
 
                 }
             }
 
         }
+//        Collections.sort(list);
+//        list.sort(book)
         System.out.println("After sorting price :");
-        System.out.println(obj);
+        System.out.println(list);
 
 
     }
